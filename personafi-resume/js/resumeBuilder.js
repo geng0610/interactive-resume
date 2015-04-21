bc7a9d53057eaf5427b78c54672a4154a8207057
Geng Tan <geng0610@users.noreply.github.com>
2015-04-21 14:37:33 -0400

    Committed by Udacity

---
+++ personafi-resume/js/resumeBuilder.js
@@ -0,0 +1,327 @@
+var profile = {
+    "profile_pic": "images/me.png",
+    "name": "Geng Tan",
+    "description": "MBA Candidate, Cornell Tech",
+    "e_mails": [
+        "geng0610@gmail.com",
+        "geng@personafi.io",
+        "gt286@cornell.edu"
+    ],
+    "personal_website": [
+        "http://personafi.io/",
+        "http://www.projectsbygeng.com/",
+        "https://www.linkedin.com/in/gengt"
+    ],
+    "contacts": {
+        "location": "new york"
+    },
+    "social":{
+        "facebook": {"link":"https://www.facebook.com/geng.tan"},
+        "linkedin": {"link":"https://www.linkedin.com/in/gengt"},
+        "twitter": {"link":"https://twitter.com/geng0610"},
+        "googleplus": {"link":"https://plus.google.com/+GengTan/"},
+        "angellist": {"link":"https://angel.co/geng-tan"},
+        "github": {"link":"https://github.com/geng0610"},
+        "instagram": {"link":"https://instagram.com/geng0610"},
+        "pinterest": {"link":"https://www.pinterest.com/geng0610/"},
+        "vine": {"link":"https://vine.co/"},
+        "quora": {"link":"http://www.quora.com/Geng-Tan"},
+        "youtube": {"link":"https://www.youtube.com/user/geng0610"}
+    }
+};
+var jobs = {
+    "jobs": [
+        {
+            "employer": "Cornell",
+            "position": "MBA Student",
+            "source": "http://tech.cornell.edu/",
+            "location": "New York",
+            "date": "2014-2015",
+            "link": "http://tech.cornell.edu/",
+            "image_link": "http://www.fallsofficial.com/files/2014/09/Cornell.png",
+            "description": "<b>Prominent Projects include</b> <li>Design and prototyping of Personafi, a people search engine that uses natural language processing, social platforms, and publicly available data to profile, tag, and find people</li><li>Design and prototyping of Roller Deck, a personal social network and media aggregator</li><li>Design and prototyping of security platform for IoT devices for Canary Smart Security</li><li>NYC Taxi Viz. Recommendation for best locations to take taxi in NYC based on data from over 170 million taxi rides</li>"
+        },
+        {
+            "employer": "Croscon",
+            "position": "MBA Intern",
+            "source": "http://www.croscon.com/",
+            "location": "New York",
+            "date": "2014",
+            "link": "http://www.croscon.com/",
+            "image_link": "https://pbs.twimg.com/profile_images/448570928700473344/h0xRp_to_400x400.png",
+            "description": "Defined several features for a project management tool and created low fidelity wireframe in Balsamiq and high fidelity wireframe using PowerPoint."
+        },
+        {
+            "employer": "Appinions",
+            "position": "Analyst",
+            "source": "http://www.appinions.com/",
+            "location": "New York",
+            "date": "2012-2014",
+            "link": "http://www.appinions.com/",
+            "image_link": "https://pbs.twimg.com/profile_images/2597231983/jkjaf08nnf2pay487zra.jpeg",
+            "description": "<b>Designed and prototyped new features and created a number of industry reports using Appinions' big-data platform</b><li>Improved company brand by collaborating with journalists and publishing reports in reputable media outlets. (Forbes, USAToday, etc)</li><li>Served as a liaison between business team and engineers in identifying high priority features</li>"
+        },
+        {
+            "employer": "The Boston Consulting Group",
+            "position": "Associate",
+            "source": "http://www.bcg.com/",
+            "location": "Los Angeles",
+            "date": "2011-2012",
+            "link": "http://www.bcg.com/",
+            "image_link": "http://utsbsoc.com/wp-content/uploads/2012/07/bcg-icon.png",
+            "description": "<b>Management consulting across a number of industries</b><li>Developed strategies to realize $80M+ in saving opportunities, identified through analysis of $4B+ procurement data for a pharmaceutical company</li><li>Identified a decreasing value perception by analyzing survey results for a consumer goods company suffering from a slowing revenue growth</li><li>Recommended growth opportunities after developing a forecast for returns across 20 U.S. housing markets for a home building company</li><li>Recommended profitability improvement initiatives by modeling financial impact of operating under various supply and demand scenarios for a major consumer goods company</li><li>Benchmarked the financial performance of $3B+ business divisions and forecasted financial impact of various initiatives for a major building products company</li>"
+        }
+    ]
+};
+var projects = {
+    "projects": [
+        {
+            "title": "NYC Taxi Viz",
+            "sponsor": "Cornell University",
+            "source": "http://www.nyctaxiviz.com",
+            "dates": "2015",
+            "link": "http://tech.cornell.edu/",
+            "image_link": "http://www.fallsofficial.com/files/2014/09/Cornell.png",
+            "description": '<div>In 2013, New Yorkers took 170 million taxi rides. The New York City Taxi Commission recently released its data under the Freedom of Information Act, resulting in our ability to use big data visualization to find patterns and insights as a project for Cornell Tech. We set out to answer an argument we&#39ve all had many times- where&#39s the best place to walk and grab a cab?</div> <div style="text-align:center"><img src="http://www.weebly.com/uploads/2/6/2/8/26283003/front-page.gif" style="width:50%"></div>'
+        },
+        {
+            "title": "People Search - Cornell Big Data Hackathon",
+            "sponsor": "Cornell University",
+            "source": "https://datahackathon2015.splashthat.com/",
+            "dates": "2015",
+            "link": "http://tech.cornell.edu/",
+            "image_link": "http://www.fallsofficial.com/files/2014/09/Cornell.png",
+            "description": '<div>During Big Data Hackathon held at Cornell Tech, we looked at the commonalities between people from CrunchBase Database and me in order to explore ways to optimize search result for a person. Characteristics about people are extracted using AlchemyAPI (NLP). This graph is based on a search result for product related people. The larger the dot, the more commonalities the person shares with me. The lines between each person represent common traists shared by them. Thicker lines means more commonalities.</div><div style="text-align:center"><div id="chart"></div></div>'
+        }
+    ]
+};
+var schools = {
+    "schools": [
+        {
+            "school": "Cornell Tech",
+            "location": "New York City, NY",
+            "degree": "MBA",
+            "source": "http://tech.cornell.edu/",
+            "dates": "2014-2015",
+            "link": "http://tech.cornell.edu/",
+            "image_link": "http://www.fallsofficial.com/files/2014/09/Cornell.png"
+        },
+        {
+            "school": "MIT",
+            "location": "Cambridge, MA",
+            "degree": "BS",
+            "source": "http://web.mit.edu/",
+            "major": "Mechanical Engineering",
+            "dates": "2006-2010",
+            "link": "http://web.mit.edu/",
+            "image_link": "http://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/MIT_logo.svg/321px-MIT_logo.svg.png"
+        },
+        {
+            "school": "MIT",
+            "location": "Cambridge, MA",
+            "degree": "BS",
+            "source": "http://web.mit.edu/",
+            "major": "Management Science",
+            "dates": "2006-2010",
+            "link": "http://web.mit.edu/",
+            "image_link": "http://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/MIT_logo.svg/321px-MIT_logo.svg.png"
+        },
+        {
+            "school": "Udacity",
+            "location": "",
+            "degree": "Nano-Degree",
+            "source": "udacity.com",
+            "major": "Web Development",
+            "dates": "2015",
+            "link": "udacity.com",
+            "image_link": "https://forums.udacity.com/upfiles/udacity-logo.jpg"
+        }
+    ]
+};
+var websites = {
+    "websites": [
+        {
+            "title": "Projects by Geng",
+            "link": "www.projectsbygeng.com/",
+            "snippet": "Welcome to Projects by Geng. Sharing my latest projects with the world! I created this website in order to ... Here are some of my coding projects. Let me see!"
+        },
+        {
+            "title": "Geng Tan - Profile | Tableau Public",
+            "link": "public.tableau.com/profile/gt286#!/edit",
+            "snippet": "Entreprenuer, Engineer and an MBA, Data viz enthusiast. I used to be an analyst at a big data start-up and BCG."
+        },
+        {
+            "title": "Tan Geng - Forbes",
+            "link": "www.forbes.com/profile/tan-geng/",
+            "snippet": "Tan Geng on Forbes. ... Tan Geng. Follow Following Unfollow (0). Profile Not Available. Financial data provided by. Forbes Video. Who Just Made a Billion ..."
+        },
+        {
+            "title": "Geng Tan | CoFoundersLab",
+            "link": "https://www.cofounderslab.com/geng-tan-57824",
+            "snippet": "Geng. i'm a: Product Manager. looking for a: Programmer/Developer. to be my: Co-Founder. City: New York, US. Industry: B2B Services. Age group: 25 - 35 ..."
+        },
+        {
+            "title": "Geng Tan's (GengTan) software Portfolio | ChallengePost",
+            "link": "challengepost.com/GengTan/challenges",
+            "snippet": "Geng Tan is a Designer specializing in Data. Follow Geng Tan on ChallengePost! ... Geng Tan's avatar. Follow. Geng Tan (GengTan). Designer, Business ..."
+        },
+        {
+            "title": "Halloween Party 2011 by Geng Tan - SoundCloud",
+            "link": "https://soundcloud.com/geng-tan/halloween-party-2011",
+            "snippet": "Follow Geng Tan and others on SoundCloud. Sign up for SoundCloud. Sign in. Here's a part of the mix I made for a Halloween party in 2011."
+        }
+    ]
+};
+var topics = {
+    "topics": [
+        {
+            "text": "NLP",
+            "relevance": "0.8"
+        },
+        {
+            "text": "Product Management",
+            "relevance": "0.8"
+        },
+        {
+            "text": "Agile Development",
+            "relevance": "0.8"
+        },
+        {
+            "text": "Strategy",
+            "relevance": "0.8"
+        },
+        {
+            "text": "Wireframe",
+            "relevance": "0.8"
+        },
+        {
+            "text": "PowerPoint",
+            "relevance": "0.8"
+        },
+        {
+            "text": "Excel",
+            "relevance": "0.8"
+        },
+        {
+            "text": "Tableau",
+            "relevance": "0.7"
+        },
+        {
+            "text": "Microsoft Office",
+            "relevance": "0.2"
+        }
+    ]
+
+};
+
+var fromTheWebColor = ["#3F51B5", "#D50000", "#109D59", "#FFC107"];
+
+profile.display = function(){
+$("#header").append(HTMLheaderPic.replace("%profile-pic%",profile.profile_pic));
+$("#header").append(HTMLheaderDetail);
+$("#header-details").append(HTMLheaderName.replace("%name%",profile.name));
+$("#header-details").append(HTMLheaderDescription.replace("%header-description%",profile.description));
+$("#header-details").append(HTMLheaderEmail);
+$("#header-details").append(HTMLheaderWebsite);
+$("#personal-e-mail").append(HTMLheaderEmailText.replace(/%personal-e-mail%/g,profile.e_mails[0]));
+$("#personal-e-mail").append(HTMLheaderEmailEnd);
+$("#personal-website").append(HTMLheaderWebsiteText.replace(/%weblink%/g,profile.personal_website[0]));
+$("#personal-website").append(HTMLheaderWebsiteEnd);
+
+if(profile.social){
+    $("#resume-main").append(HTMLsocialIcons);
+    for (network in profile.social) {
+        $("#social-icons").append(HTMLsocialIcon.replace(/%social%/g,network).replace("%link%",profile.social[network].link));
+    }
+}
+}
+
+profile.display();
+
+jobs.display = function(){
+$("#resume-main").append(HTMLworkExperiences);
+for (job in jobs.jobs){
+    $("#work-experiences").append(HTMLworkExperienceStart
+        +HTMLworkExperienceLink.replace("%link%",jobs.jobs[job].link).replace("%image-source%",jobs.jobs[job].image_link)
+        +HTMLworkExperienceSource.replace("%source%",jobs.jobs[job].source)
+        +HTMLworkExperiencePosition.replace("%position%",jobs.jobs[job].position)
+        +HTMLworkExperienceEmployer.replace("%employer%",jobs.jobs[job].employer)
+        +HTMLworkExperienceDate.replace("%date%",jobs.jobs[job].date)
+        +HTMLworkExperienceDescription.replace("%description%",jobs.jobs[job].description));
+}
+$(".entry-text").last()[0].getElementsByTagName('hr')[0].remove();
+}
+
+jobs.display();
+
+
+projects.display = function (){
+$("#resume-main").append(HTMLprojectExperiences);
+for (project in projects.projects){
+    $("#project-experiences").append(HTMLprojectExperienceStart
+        +HTMLprojectExperienceLink.replace("%link%",projects.projects[project].link).replace("%image-source%",projects.projects[project].image_link)
+        +HTMLprojectExperienceSource.replace("%source%",projects.projects[project].source)
+        +HTMLprojectExperienceTitle.replace("%title%",projects.projects[project].title)
+        +HTMLprojectExperienceSponsor.replace("%sponsor%",projects.projects[project].sponsor)
+        +HTMLprojectExperienceDate.replace("%date%",projects.projects[project].dates)
+        +HTMLprojectExperienceDescription.replace("%description%",projects.projects[project].description));
+}
+$(".entry-text").last()[0].getElementsByTagName('hr')[0].remove();
+}
+
+projects.display();
+
+schools.display = function (){
+$("#resume-main").append(HTMLeducationExperiences);
+for (school in schools.schools){
+    var major = "";
+    if(schools.schools[school].major){
+        major=HTMLeducationExperienceMajor.replace("%major%",schools.schools[school].major);
+    }
+    $("#education-experiences").append(HTMLeducationExperienceStart
+        +HTMLeducationExperienceLink.replace("%link%",schools.schools[school].link).replace("%image-source%",schools.schools[school].image_link)
+        +HTMLeducationExperienceSource.replace("%source%",schools.schools[school].source)
+        +HTMLeducationExperienceDegree.replace("%degree%",schools.schools[school].degree)
+        +major
+        +HTMLeducationExperienceSchool.replace("%school%",schools.schools[school].school)
+        +HTMLeducationExperienceDate.replace("%date%",schools.schools[school].dates));
+}
+}
+
+schools.display();
+
+
+topics.display=function(){
+$("#resume-main").append(HTMLrelevantTopics);
+for (topic in topics.topics) {
+    if(topics.topics[topic].relevance/1>0.5){
+        $("#topicList").append(HTMLrelevantTopic.replace("%topic%",topics.topics[topic].text));
+    }
+}
+}
+topics.display();
+
+websites.display=function(){
+$("#resume-main").append(HTMLfromTheWebSection);
+for(website in websites.websites) {
+    $("#fromTheWebSection").append(HTMLfromTheWebEntry.replace("%color%",fromTheWebColor[website%4]));
+    $(".web-entry").last().append(HTMLfromTheWebTitle.replace("%web-url%",websites.websites[website].link).replace("%link-title%",websites.websites[website].title));
+    $(".web-entry").last().append(HTMLfromTheWebLink.replace("%web-url%",websites.websites[website].link));
+    $(".web-entry").last().append(HTMLfromTheWebSnippet.replace("%web-site-snippet%",websites.websites[website].snippet));
+}
+}
+
+websites.display();
+
+$("#resume-main").append(HTMLmap);
+$("#mapDiv").append(googleMap);
+
+profile.displayFooter=function(){
+if(profile.social){
+    $("#resume-main").append(HTMLsocialIconsBelow);
+    for (network in profile.social) {
+        $("#social-icons-below").append(HTMLsocialIcon.replace(/%social%/g,network).replace("%link%",profile.social[network].link));
+    }
+}
+}
+profile.displayFooter();
